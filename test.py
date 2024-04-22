463ba0aab84dee1063250ad75a7d6725bc5ff218
GuillaumeFalourd <guillaume.falourd@zup.com.br>
2024-04-22 17:43:30 -0300

    add text

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from flask import Flask, request, render_template_string
 
 app = Flask(__name__)
 
-# SQL Injection Vulnerability Test 3
+# SQL Injection Vulnerability Test 4
 @app.route('/login')
 def login():
     username = request.args.get('username')
